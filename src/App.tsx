2ab3063486700d3d7007e4b38370b044c5711030
Daniel Carvalho <daniel.devop@gmail.com>
2024-02-25 10:18:04 -0300

    estrutura básica

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import {Plus} from 'lucide-react'
+
 export function App() {
   return (
-    <>
-      <h1>Olá mundo</h1>
-    </>
+    <div className="py-10 space-y-8">
+      <div>
+        header
+        tabs
+      </div>
+      <main className="max-w-6xl mx-auto space-y-5">
+        <div className='flex gap-3 items-center'>
+          <h1 className="text-xl font-bold">Tags</h1>
+          <button className='inline-flex items-center gap-1.5 text-xs bg-teal-300 text-teal-950 font-medium rounded-full px-1.5 py-1'>
+            <Plus className='size-3' />
+            Create new
+          </button> 
+        </div>
+      </main>
+    </div>
   )
 }
