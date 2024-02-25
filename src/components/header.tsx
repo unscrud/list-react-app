a4371b950a37a097c3d01d592d13d5caf343c779
Daniel Carvalho <daniel.devop@gmail.com>
2024-02-25 12:30:28 -0300

    estrutura

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,11 @@
 export function Header() {
     return (
-        <div>
-            HEADER
+        <div className="max-w-[1200px] mx-auto flex items-center justify-between">
+            <div className=" flex items-center gap-3">
+                <div className=" flex items-center gap-2.5">
+                
+                </div>
+            </div>
         </div>
     )
 }
