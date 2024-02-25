1702885402dfadc0f85c644eb1f8976077b74ca8
Daniel Carvalho <daniel.devop@gmail.com>
2024-02-25 13:14:18 -0300

    usando variants do tailwind

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -1,17 +1,25 @@
 import { tv, VariantProps } from "tailwind-variants";
-
 import { ComponentProps } from "react";
 
 const badge = tv({
     base: 'inline-block py-1 px-2 rounded-full font-semibold text-[0.625rem]',
 
-    variants: {}
+    variants: {
+        variant: {
+            ghost: 'bg-zinc-800 text-zinc-500',
+            primary: 'bg-teal-950 text-teal-300'
+        }
+    },
+
+    defaultVariants: {
+        variant: 'ghost'
+    }
 })
 
 interface BadgeProps
     extends ComponentProps<'span'>,
         VariantProps<typeof badge> {}
 
-export function Badge({...props}: BadgeProps) {
-    return <span {...props} />
+export function Badge({className, variant, ...props}: BadgeProps) {
+    return <span className={badge({variant, className})} {...props} />
 }
