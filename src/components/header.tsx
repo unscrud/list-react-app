fc6fc734176e6228c763287eb705e03b925bc2c5
Daniel Carvalho <daniel.devop@gmail.com>
2024-02-25 13:18:57 -0300

    barra ao lado do badge

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -10,6 +10,24 @@ export function Header() {
 
                     <Badge>BETA</Badge>
                 </div>
+
+                <svg
+                    width="6"
+                    height="16"
+                    viewBox="0 0 6 16"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <line 
+                        x1="1.18372"
+                        y1="15.598"
+                        x2="5.32483"
+                        y2="0.143194"
+                        className="stroke-zinc-700"
+                    />
+                </svg>
+
+                
             </div>
         </div>
     )
