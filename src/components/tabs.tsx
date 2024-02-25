d67cb3d695e489555d0121a8219e878b5d2b41a9
Daniel Carvalho <daniel.devop@gmail.com>
2024-02-25 15:06:31 -0300

    tabs

--- src/components/tabs.tsx
+++ src/components/tabs.tsx
@@ -1,7 +1,27 @@
+import {ListVideo, Tags, Settings, Code2} from 'lucide-react'
+
 export function Tabs() {
     return (
         <div>
-            TABS
+            <div className="border-b border-zinc-800 py-4">
+                <nav className="flex items-center gap-2 max-w-[1200px] mx-auto">
+                    <a href="" className="px-3 py-1.5 bg-zinc-800 text-zinc-100 inline-flex items-center text-sm gap-1.5 font-medium rounded-full border border-transparent">
+                        <ListVideo className='size-4' />
+                        Uploads</a>
+                    <a href="" className="py-1.5 px-3 text-zinc-300 inline-flex items-center text-sm gap-1.5 font-medium rounded-full border border-transparent hover:border-zinc-800">
+                        <Tags />    
+                        Tags
+                    </a>
+                    <a href="" className="py-1.5 px-3 text-zinc-300 inline-flex items-center text-sm gap-1.5 font-medium rounded-full border border-transparent hover:border-zinc-800">
+                        <Settings />    
+                        Settings
+                    </a>
+                    <a href="" className="py-1.5 px-3 text-zinc-300 inline-flex items-center text-sm gap-1.5 font-medium rounded-full border border-transparent hover:border-zinc-800">
+                        <Code2 />    
+                        Developers
+                    </a>
+                </nav>
+            </div>
         </div>
     )
 }
