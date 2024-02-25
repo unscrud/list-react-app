b29ade11f78389ea52f586d3691261ac0b167623
Daniel Carvalho <daniel.devop@gmail.com>
2024-02-25 12:32:12 -0300

    logo image

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,11 @@
+import nivoLogo from '../assets/logo-nivo.svg'
+
 export function Header() {
     return (
         <div className="max-w-[1200px] mx-auto flex items-center justify-between">
             <div className=" flex items-center gap-3">
                 <div className=" flex items-center gap-2.5">
-                
+                    <img src={nivoLogo} alt="nivo.video" />
                 </div>
             </div>
         </div>
